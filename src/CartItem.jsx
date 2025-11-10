fa5bbb773d24e34bf2fbc824301c4e6396e95f62
Riddhi Jha <riddhij2@gmail.com>
2025-11-10 13:49:20 -0500

    Added TotalAmount

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,20 +9,27 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach((item) => {
+        total += parseFloat(item.cost.substring(1)) * item.quantity;
+    }) 
+    return total;
   };
 
-  const handleContinueShopping = (e) => {
-   
+  const handleContinueShopping = (e) => {   
   };
 
 
 
   const handleIncrement = (item) => {
+    
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity > 0){
+        dispatch(removeItem(item))
+    }
+    
   };
 
   const handleRemove = (item) => {
